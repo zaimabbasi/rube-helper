21218ab1173c97a6945eefc4970f54b2d3cecbb4
Zaim Abbasi <xayim@yahoo.com>
2017-09-08 16:31:23 +0500

    initial commit

---
+++ RawJsonConverter/json_converter.cpp
@@ -0,0 +1,9 @@
+//
+//  json_converter.c
+//  RawJsonConverter
+//
+//  Created by Zaim Abbasi on 28/08/2017.
+//  Copyright © 2017 Zaim Abbasi. All rights reserved.
+//
+
+#include "json_converter.h"
---
+++ RawJsonConverter/json_converter.h
@@ -0,0 +1,14 @@
+//
+//  json_converter.h
+//  RawJsonConverter
+//
+//  Created by Zaim Abbasi on 28/08/2017.
+//  Copyright © 2017 Zaim Abbasi. All rights reserved.
+//
+
+#ifndef json_converter_h
+#define json_converter_h
+
+#include <stdio.h>
+
+#endif /* json_converter_h */
