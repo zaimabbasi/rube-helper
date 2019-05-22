7c00d20771e6ca226300f655e84663086557b5b9
Zaim Abbasi <xayim@yahoo.com>
2019-05-23 00:02:38 +0500

    RubeHelper

---
+++ RubeHelper/main.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include "rube_helper.h"
+
+#define BUILD_ALL   1
+
+#define WORLD_COUNT 1
+#define LEVEL_COUNT 1
+
+int main(int argc, const char * argv[]) {
+    
+    std::string res_path;
+    std::string tar_path;
+    
+    if (!BUILD_ALL)
+    {
+        int world_id = 2;
+        int level_id = 4;
+        
+        res_path = "res/level_"+std::to_string(world_id)+"_"+std::to_string(level_id)+".json";
+        tar_path = "tar/level_"+std::to_string(world_id)+"_"+std::to_string(level_id)+".json";
+        
+        RubeHelper::parse_level(res_path, tar_path, world_id, level_id);
+    }
+    else
+    {
+        for (int i = 0; i < WORLD_COUNT; ++i)
+        {
+            for (int j = 0; j < LEVEL_COUNT; ++j)
+            {
+                res_path = "res/level_"+std::to_string(i)+"_"+std::to_string(j)+".json";
+                tar_path = "tar/level_"+std::to_string(i)+"/level_"+std::to_string(j)+".json";
+                
+                RubeHelper::parse_level(res_path, tar_path, i, j);
+            }
+        }
+    }
+    
+    return 0;
+}
